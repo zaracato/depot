3160eb3aab6b93f031e184965094f0aa6063dd6b
Arturo Hernandez <zaracato@gmail.com>
2011-11-30 22:43:17 -0600

    chap 12 - mail

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,20 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Depot::Application.initialize!
+
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings ={
+      address: "stmp.gmail.com",
+      port: 587,
+      domain: "domain.of.sender.net",
+      authentication: "plain",
+      user_name: "zaracato",
+      password: "Gr1ll1t0",
+      enable_starttls_auto: true
+
+
+
+  }
+end
+
