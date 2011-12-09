971275d163aefd604bbe35b3dc8c76bfecf13b50
Arturo Hernandez Lomeli <zaracato@gmail.com>
2011-12-09 09:53:48 -0600

    autentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,25 @@
 Depot::Application.routes.draw do
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+  #get "admin/index"
+
+  #get "sessions/new"
+
+  #get "sessions/create"
+
+  #get "sessions/destroy"
+
+  resources :users
+
   resources :orders
 
   resources :line_items do
-  member do
+     member do
       post 'decrement'
     end
   end
