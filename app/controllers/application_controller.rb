0fbc82a5b5b30e4dea420bae0af823d17dedebca
Arturo Hernandez Lomeli <zaracato@gmail.com>
2011-12-12 11:10:05 -0600

    chap 13

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+ before_filter :authorize
+ # protect_from_forgery
 
  private
    def current_cart
@@ -9,4 +10,19 @@ class ApplicationController < ActionController::Base
     session[:cart_id] = cart.id
     cart
    end
+
+  protected
+  def authorize
+     unless User.find_by_id(session[:user_id])
+       if User.count.zero?
+         if not((request.path_parameters[:action]=="new" or  request.path_parameters[:action]=="create" )and request.path_parameters[:controller]=="users")
+           redirect_to(:controller => "users", :action => "new")
+         end
+       else
+
+          redirect_to login_url, notice: "please log in"
+       end
+
+     end
+  end
 end
